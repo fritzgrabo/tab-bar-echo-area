4865541e2273674ec4343551457f76d4629a79ab
Fritz Grabo <fritz.grabo@gmail.com>
2021-05-03 08:31:17 +0200

    Major refactor to use the tab bar keymap as the source of tabs to display

--- tab-bar-echo-area.el
+++ tab-bar-echo-area.el
@@ -26,94 +26,227 @@
 ;;; Commentary:
 
 ;; Provides a global minor mode to temporarily display a list of
-;; available tab names (with the current tab's name highlighted) in the
-;; echo area after tab-related commands.
-
-;; The list of tab names shows after creating, closing, switching to,
-;; and renaming a tab, and remains visible until the next command is
-;; issued.
+;; available tabs and tab groups (with the current tab and group
+;; highlighted) in the echo area after tab-related commands.
 
 ;; This is intended to be used as an unobtrusive replacement for the
-;; Emacs built-in display of the tab-bar (that is, when you have
+;; Emacs built-in display of the tab bar (that is, when you have
 ;; `tab-bar-show' set to nil).
 
-;; The idea is to provide but a quick visual orientation aid to the user
-;; after tab-related commands, and then get out of the way again.
+;; The idea is to provide but a quick visual orientation aid to the
+;; user after tab-related commands, and then get out of the way again.
 
-;; I recommend using this in combination with the tab-bar-lost-commands
-;; package, which provides simple and convenient commands that help with
-;; common tab bar use-cases regarding the creation, selection and
+;; I recommend using this together with the 'tab-bar-lost-commands'
+;; package, which provides simple and convenient commands that help
+;; with common tab bar use-cases regarding the creation, selection and
 ;; movement of tabs.
 
+;; You might also want to check out the 'tab-bar-groups' package, which
+;; backports a simplified version of Emacs 28 tab groups to Emacs 27 and
+;; provides an integration with this package.
+
 ;;; Code:
+
+(require 'seq)
 
 (eval-when-compile (require 'subr-x))
 
 (defgroup tab-bar-echo-area ()
-  "Display tab names of the tab bar in the echo area."
+  "Display tabs of the tab bar in the echo area."
   :group 'tab-bar)
 
-(defface tab-bar-echo-area-current-tab
-  '((t :inherit bold))
-  "Face to use for the current tab name.")
+(defface tab-bar-echo-area-tab
+  '((t :inverse-video t))
+  "Tab bar echo area face for selected tab.")
 
-(defface tab-bar-echo-area-other-tab
+(defface tab-bar-echo-area-tab-inactive
+  '((t))
+  "Tab bar echo area face for non-selected tab.")
+
+(defface tab-bar-echo-area-tab-ungrouped
   '((t :inherit shadow))
-  "Face to use for tab names except the current one.")
+  "Tab bar echo area face for ungrouped tab when tab groups are used.")
+
+(defface tab-bar-echo-area-tab-group-current
+  '((t :inherit (underline bold)))
+  "Tab bar echo area face for current group tab.")
+
+(defface tab-bar-echo-area-tab-group-inactive
+  '((t :inherit underline))
+  "Tab bar echo area face for inactive group tab.")
 
 (defvar tab-bar-echo-area-trigger-display-functions
-  '(tab-bar-close-tab
+  '(tab-bar-close-tab ;; Emacs 27 and higher
     tab-bar-move-tab-to
     tab-bar-new-tab-to
     tab-bar-rename-tab
     tab-bar-select-tab
-    display-buffer-in-new-tab)
-  "List of functions after which to display tab names in the echo area.")
+    display-buffer-in-new-tab ;; Emacs 28 and higher
+    tab-bar-change-tab-group)
+  "List of functions after which to display tabs in the echo area.")
 
-(defvar tab-bar-echo-area-process-tab-name-functions
-  (list #'tab-bar-echo-area-propertize-tab-name)
-  "List of functions to call to fully process a tab's name for display.
+(defvar tab-bar-echo-area-style-tab-name-functions
+  '(tab-bar-echo-area-remove-tab-name-properties
+    tab-bar-echo-area-propertize-tab-name)
+  "List of functions to call to style a tab's name for display.
 
-Each function is expected to take a TAB-NAME and its TAB as
-arguments, and to return TAB-NAME further processed for
-display.")
+Each function is expected to take NAME, TYPE, TAB, INDEX and
+COUNT as arguments, and to return a copy of the name that was
+further styled for display.
 
-(defun tab-bar-echo-area-propertize-tab-name (tab-name tab)
-  "Propertize TAB-NAME for TAB."
-  (let ((face (if (equal (car tab) 'current-tab)
-                  'tab-bar-echo-area-current-tab
-                'tab-bar-echo-area-other-tab)))
-    (font-lock-append-text-property 0 (length tab-name) 'face face tab-name))
-  tab-name)
+NAME is the partially styled tab's name as provided by the
+previous function in the list.  In Emacs 27, TYPE is either 'tab
+or 'current-tab.  In Emacs 28 and higher, it may also be 'group
+or 'current-group.  TAB is the tab that the name belongs to.
+INDEX is the index of the tab within the list of displayed tabs.
+COUNT is the total number of displayed tabs.")
 
-(defun tab-bar-echo-area-processed-tab-names ()
-  "Generate a list of processed tab names."
-  (mapcar #'tab-bar-echo-area-process-tab-name (funcall tab-bar-tabs-function)))
+(defvar tab-bar-echo-area-format-tab-name-functions
+  '(tab-bar-echo-area-format-tab-name-for-joining)
+  "List of functions to call to format a styled tab name for display.
 
-(defun tab-bar-echo-area-process-tab-name (tab)
-  "Process tab name for TAB."
-  (let ((tab-name (concat (alist-get 'name tab))))
-    (dolist (f tab-bar-echo-area-process-tab-name-functions)
-      (setq tab-name (funcall f tab-name tab)))
-    tab-name))
+See `tab-bar-echo-area-style-tab-name-functions' for a list and
+a description of the arguments passed into these functions.")
+
+(defvar tab-bar-echo-area-format
+  nil
+  "Optional customization of `tab-bar-format' in the context of the echo area.")
+
+;; --- Keymap handling
+
+(defvar tab-bar-echo-area-make-keymap-function
+  #'tab-bar-echo-area-make-keymap
+  "Function to make the keymap used as the source of tabs to display.")
+
+(defun tab-bar-echo-area-make-keymap ()
+  "Make a keymap to use as the source of tabs to display."
+  (let ((tab-bar-close-button-show nil)
+        (tab-bar-tab-hints nil))
+    (tab-bar-make-keymap)))
+
+(defvar tab-bar-echo-area--keymap-element-type-regex
+  "^\\(group\\|\\(?:current-\\)?tab\\)\\(?:-\\([[:digit:]]+\\)\\)?$"
+  "Regex to detect relevant tab bar keymap elements by their type.
+
+The Regex must provide at least two match groups.  The first
+match group must match the actual type (without the index part)
+of a relevant tab bar keymap element.  By default, that is 'tab
+and 'current-tab in Emacs 27 and additionally, 'group in Emacs 28
+and higher.  The second match group must match the index of the
+element in the keymap, if any.  For example, a keymap element
+with a type of 'tab-4 should match 'tab' and '4'.
+
+Only match types of keymap elements that you want to be fed into
+`tab-bar-echo-area-style-tab-name-functions' and
+`tab-bar-echo-area-format-tab-name-functions'.")
+
+(defun tab-bar-echo-area--keymap-element-type (keymap-element)
+  "Extract the actual type of KEYMAP-ELEMENT.
+
+Returns either 'tab or 'current-tab in Emacs 27 and additionally,
+'group or 'current-group in Emacs 28 and higher."
+  (when-let* ((raw-type-string (symbol-name (car keymap-element)))
+              (type (and (string-match tab-bar-echo-area--keymap-element-type-regex raw-type-string)
+                         (intern (match-string 1 raw-type-string))))
+              (name (caddr keymap-element)))
+    (if (and (eq type 'group)
+             (string= name (alist-get 'group (tab-bar--current-tab))))
+        'current-group
+      type)))
+
+(defun tab-bar-echo-area--keymap-element-tab (keymap-element)
+  "Find the tab that KEYMAP-ELEMENT relates to.
+
+In Emacs 28 and higher, for keymap elements that denote tab
+groups (that is, keymap elements that have a type of 'group-*),
+return the first tab in the group.
+
+If the keymap element does not relate to a tab, return nil."
+  (let ((raw-type (car keymap-element)))
+    (if (eq raw-type 'current-tab)
+        (tab-bar--current-tab)
+      (let ((raw-type-string (symbol-name raw-type)))
+        (string-match tab-bar-echo-area--keymap-element-type-regex raw-type-string)
+        (if (member (match-string 1 raw-type-string) '("tab" "group"))
+            (nth (1- (string-to-number (match-string 2 raw-type-string)))
+                 (funcall tab-bar-tabs-function)))))))
+
+;; --- Tab name processing
+
+(defun tab-bar-echo-area-remove-tab-name-properties (name _type _tab _index _count)
+  "Remove all text properties from NAME."
+  (substring-no-properties name))
+
+(defun tab-bar-echo-area-propertize-tab-name (name type tab _index _count)
+  "Propertize NAME according to TYPE and TAB."
+  (let* ((name (concat name))
+         (face (cond ((eq type 'current-tab) 'tab-bar-echo-area-tab)
+                     ((eq type 'tab) (if (alist-get 'group tab) 'tab-bar-echo-area-tab-inactive 'tab-bar-echo-area-tab-ungrouped))
+                     ((eq type 'current-group) 'tab-bar-echo-area-tab-group-current)
+                     ((eq type 'group) 'tab-bar-echo-area-tab-group-inactive))))
+    (font-lock-append-text-property 0 (length name) 'face face name)
+    name))
+
+(defun tab-bar-echo-area-format-tab-name-for-joining (name type _tab index count)
+  "Format NAME according to TYPE, INDEX and COUNT."
+  (format (cond ((eq type 'current-group) "%s ")
+                ((eq index (1- count)) "%s")
+                (t "%s, "))
+          name))
+
+(defun tab-bar-echo-area--process-tab-name (name type tab index count)
+  "Process NAME according to TYPE, TAB, INDEX, COUNT."
+  (seq-reduce
+   (lambda (name f) (funcall f name type tab index count))
+   (append
+    tab-bar-echo-area-style-tab-name-functions
+    tab-bar-echo-area-format-tab-name-functions)
+   name))
+
+(defun tab-bar-echo-area--processed-tab-names ()
+  "Generate a list of fully processed tab names for display in the echo area."
+  (let* ((tab-bar-format (or tab-bar-echo-area-format tab-bar-format))
+         (keymap (funcall tab-bar-echo-area-make-keymap-function))
+         (keymap-elements (seq-filter #'tab-bar-echo-area--keymap-element-type (cdr keymap)))
+         (keymap-elements-count (length keymap-elements)))
+    (seq-map-indexed (lambda (keymap-element index)
+                       (funcall #'tab-bar-echo-area--process-tab-name
+                                (caddr keymap-element) ;; name
+                                (tab-bar-echo-area--keymap-element-type keymap-element) ;; type
+                                (tab-bar-echo-area--keymap-element-tab keymap-element) ;; tab
+                                index ;; index
+                                keymap-elements-count)) ;; count
+                     keymap-elements)))
+
+;; --- Commands
 
 ;;;###autoload
-(defun tab-bar-echo-area-display-tab-names (&rest _args)
-  "Display all tab names in the echo area."
+(defun tab-bar-echo-area-display-tab-names ()
+  "Display tab names in the echo area."
   (interactive)
-  (message "Tabs: %s" (string-join (tab-bar-echo-area-processed-tab-names) ", ")))
+  (if-let ((tab-names (tab-bar-echo-area--processed-tab-names)))
+      (message "Tabs: %s" (string-join tab-names))))
 
 ;;;###autoload
-(defalias 'tab-bar-echo-area-print-tab-names 'tab-bar-echo-area-display-tab-names)
+(defalias 'tab-bar-echo-area-print-tab-names 'tab-bar-echo-area-display-tab-names) ;; v0.1, deprecated
 
 ;;;###autoload
 (defun tab-bar-echo-area-display-tab-name ()
   "Display the current tab's name in the echo area."
   (interactive)
-  (message "Current Tab: %s" (tab-bar-echo-area-process-tab-name (tab-bar--current-tab))))
+  (let* ((keymap-element (assoc 'current-tab (cdr (funcall tab-bar-echo-area-make-keymap-function))))
+         (name (funcall #'tab-bar-echo-area--process-tab-name
+                        (caddr keymap-element) ;; name
+                        (tab-bar-echo-area--keymap-element-type keymap-element) ;; type
+                        (tab-bar-echo-area--keymap-element-tab keymap-element) ;; tab
+                        0 ;; index
+                        1))) ;; count
+    (message "Current Tab: %s" name)))
 
 ;;;###autoload
-(defalias 'tab-bar-echo-area-print-tab-name 'tab-bar-echo-area-display-tab-name)
+(defalias 'tab-bar-echo-area-print-tab-name 'tab-bar-echo-area-display-tab-name) ;; v0.1, deprecated
+
+;; --- Wiring
 
 (defun tab-bar-echo-area-display-tab-names-advice (orig-fun &rest args)
   "Call ORIG-FUN with ARGS, then display tab names in the echo area."
@@ -121,9 +254,11 @@ display.")
     (tab-bar-echo-area-display-tab-names)
     result))
 
+;; --- Mode definition
+
 ;;;###autoload
 (define-minor-mode tab-bar-echo-area-mode
-  "Alternative to `tab-bar-mode': display tab names in the echo area after tab bar-related functions."
+  "Alternative to function `tab-bar-mode': display tab names in the echo area after tab bar-related functions."
   :group 'tab-bar
   :global t
   (tab-bar-echo-area-apply-display-tab-names-advice))
